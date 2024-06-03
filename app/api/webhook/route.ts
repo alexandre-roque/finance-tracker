976f3e208b6870fcf5bb8d89d3a32f76078c6b85
Alexandre Roque <alexandre.roque1@hotmail.com>
2024-06-02 22:55:30 -0300

    feat: adicionar resposta ao webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,4 +2,6 @@ export async function POST(request: Request) {
 	console.log(request);
 	const body = await request.json();
 	console.log(body);
+
+	return Response.json({ success: true }, { status: 200 });
 }

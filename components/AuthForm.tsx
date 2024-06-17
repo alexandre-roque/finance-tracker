44392b6c0f3c6a2b26f4a8ebbb475fffe270468b
Alexandre Roque <alexandre.roque@sydle.com>
2024-06-17 17:20:15 -0300

    feat: enviar formulário de login ao dar enter

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -7,7 +7,7 @@ import { signIn } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Loader2 } from 'lucide-react';
 import CustomInput from '@/components/CustomInput';
 import { z } from 'zod';
@@ -15,96 +15,104 @@ import { authFormSchema } from '@/schemas';
 import Link from 'next/link';
 
 export default function AuthForm({ type }: { type: 'sign-in' | 'sign-up' }) {
-	const router = useRouter();
-	const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
 
-	const formSchema = authFormSchema(type);
+    const formSchema = authFormSchema(type);
 
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			email: '',
-			password: '',
-			name: '',
-		},
-	});
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+            name: '',
+        },
+    });
 
-	const onSubmit = async (values: z.infer<typeof formSchema>) => {
-		setIsLoading(true);
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        setIsLoading(true);
 
-		if (type === 'sign-in') {
-			const result = await signIn('credentials', {
-				email: values.email,
-				password: values.password,
-				redirect: false,
-			});
+        if (type === 'sign-in') {
+            const result = await signIn('credentials', {
+                email: values.email,
+                password: values.password,
+                redirect: false,
+            });
 
-			if (result?.error) {
-				toast.error('Usuário não encontrado ou credenciais incorretas!');
-			} else {
-				router.push('/');
-			}
-		} else {
-			const result = await fetch('/api/user', {
-				method: 'POST',
-				body: JSON.stringify(values),
-			}).then((res) => res.json());
+            if (result?.error) {
+                toast.error('Usuário não encontrado ou credenciais incorretas!');
+            } else {
+                router.push('/');
+            }
+        } else {
+            const result = await fetch('/api/user', {
+                method: 'POST',
+                body: JSON.stringify(values),
+            }).then((res) => res.json());
 
-			if (result.errorMessage) {
-				if (result.errorMessage.name === 'LibsqlError') {
-					toast.error('Email duplicado, favor usar outro');
-				} else {
-					toast.error('Ocorreu um erro, tente novamente!');
-				}
-			} else {
-				await signIn('credentials', {
-					email: values.email,
-					password: values.password,
-					callbackUrl: '/',
-				});
-			}
-		}
-		setIsLoading(false);
-	};
+            if (result.errorMessage) {
+                if (result.errorMessage.name === 'LibsqlError') {
+                    toast.error('Email duplicado, favor usar outro');
+                } else {
+                    toast.error('Ocorreu um erro, tente novamente!');
+                }
+            } else {
+                await signIn('credentials', {
+                    email: values.email,
+                    password: values.password,
+                    callbackUrl: '/',
+                });
+            }
+        }
+        setIsLoading(false);
+    };
 
-	return (
-		<div>
-			<Form {...form}>
-				<form className='space-y-4' onSubmit={form.handleSubmit(onSubmit)}>
-					{type === 'sign-up' && (
-						<>
-							<CustomInput
-								control={form.control}
-								name='name'
-								label='Nome'
-								placeholder='Digite seu nome'
-							/>
-						</>
-					)}
+    useEffect(() => {
+        const handleKeyDown = (event: KeyboardEvent) => {
+            if (event.key === 'Enter') {
+				form.handleSubmit(onSubmit)();
+            }
+        };
 
-					<CustomInput control={form.control} name='email' label='Email' placeholder='Digite seu e-mail' />
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
 
-					<CustomInput
-						control={form.control}
-						name='password'
-						label='Password'
-						placeholder='Digite sua senha'
-						type='password'
-					/>
-				</form>
-			</Form>
-			<Button disabled={isLoading} onClick={form.handleSubmit(onSubmit)} className='w-full mt-6'>
-				{isLoading ? <Loader2 className='animate-spin' /> : type === 'sign-in' ? 'Log in' : 'Cadastrar'}
-			</Button>
+    return (
+        <div>
+            <Form {...form}>
+                <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+                    {type === 'sign-up' && (
+                        <>
+                            <CustomInput control={form.control} name="name" label="Nome" placeholder="Digite seu nome" />
+                        </>
+                    )}
 
-			<footer className='flex justify-center gap-2 mt-4'>
-				<p className='text-14 font-normal text-gray-600'>
-					{type === 'sign-in' ? 'Deseja realizar cadastro?' : 'Já possui uma conta?'}
-				</p>
-				<Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className='form-link'>
-					{type === 'sign-in' ? 'Cadastrar' : 'Log in'}
-				</Link>
-			</footer>
-		</div>
-	);
+                    <CustomInput control={form.control} name="email" label="Email" placeholder="Digite seu e-mail" />
+
+                    <CustomInput
+                        control={form.control}
+                        name="password"
+                        label="Password"
+                        placeholder="Digite sua senha"
+                        type="password"
+                    />
+                </form>
+            </Form>
+            <Button disabled={isLoading} onClick={form.handleSubmit(onSubmit)} className="w-full mt-6">
+                {isLoading ? <Loader2 className="animate-spin" /> : type === 'sign-in' ? 'Log in' : 'Cadastrar'}
+            </Button>
+
+            <footer className="flex justify-center gap-2 mt-4">
+                <p className="text-14 font-normal text-gray-600">
+                    {type === 'sign-in' ? 'Deseja realizar cadastro?' : 'Já possui uma conta?'}
+                </p>
+                <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className="form-link">
+                    {type === 'sign-in' ? 'Cadastrar' : 'Log in'}
+                </Link>
+            </footer>
+        </div>
+    );
 }

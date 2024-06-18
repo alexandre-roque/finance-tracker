36b0baf1fac7907cf2da9cf80172c1ee3a4f1257
Alexandre Roque <alexandre.roque1@hotmail.com>
2024-06-17 23:48:45 -0300

    feat: possibilitar editar usuário cadastrado

---
+++ app/(root)/_actions/user.ts
@@ -0,0 +1,18 @@
+'use server';
+
+import { auth } from '@/auth';
+import { db } from '@/db';
+import { users } from '@/db/schema/users';
+import { eq } from 'drizzle-orm';
+import { redirect } from 'next/navigation';
+
+export async function EditUser({ avatarLink, name }: { avatarLink: string; name: string }) {
+	const session = await auth();
+	if (!session || !session.user || !session.user.id) {
+		redirect('/sign-in');
+	}
+
+	const userId = session.user.id;
+
+	await db.update(users).set({ image: avatarLink, name: name }).where(eq(users.id, userId));
+}

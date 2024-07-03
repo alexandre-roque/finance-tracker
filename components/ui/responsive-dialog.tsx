959bba45f0733327e66232945711dff90dcb1812
Alexandre Roque <alexandre.roque@sydle.com>
2024-07-03 20:53:05 -0300

    fix: ajustar tamanho do drawer e dialog

--- components/ui/responsive-dialog.tsx
+++ components/ui/responsive-dialog.tsx
@@ -1,73 +1,69 @@
 import * as React from 'react';
 
 import { Button } from '@/components/ui/button';
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
-} from '@/components/ui/dialog';
-import {
-	Drawer,
-	DrawerClose,
-	DrawerContent,
-	DrawerDescription,
-	DrawerFooter,
-	DrawerHeader,
-	DrawerTitle,
-	DrawerTrigger,
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
 } from '@/components/ui/drawer';
 import { useMediaQuery } from '@/hooks/use-media-query';
+import { ScrollArea } from './scroll-area';
 
 export function ResponsiveDialog({
-	children,
-	isOpen,
-	setIsOpen,
-	title,
-	description,
-	trigger,
+    children,
+    isOpen,
+    setIsOpen,
+    title,
+    description,
+    trigger,
 }: {
-	children: React.ReactNode;
-	isOpen: boolean;
-	setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
-	title: string;
-	description?: string;
-	trigger?: React.ReactNode;
+    children: React.ReactNode;
+    isOpen: boolean;
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    title: string;
+    description?: string;
+    trigger?: React.ReactNode;
 }) {
-	const isDesktop = useMediaQuery('(min-width: 768px)');
+    const isDesktop = useMediaQuery('(min-width: 768px)');
 
-	if (isDesktop) {
-		return (
-			<Dialog open={isOpen} onOpenChange={setIsOpen}>
-				{trigger && <DialogTrigger>{trigger}</DialogTrigger>}
-				<DialogContent className='sm:max-w-[425px]'>
-					<DialogHeader>
-						<DialogTitle>{title}</DialogTitle>
-						{description && <DialogDescription>{description}</DialogDescription>}
-					</DialogHeader>
-					{children}
-				</DialogContent>
-			</Dialog>
-		);
-	}
+    if (isDesktop) {
+        return (
+            <Dialog open={isOpen} onOpenChange={setIsOpen}>
+                {trigger && <DialogTrigger>{trigger}</DialogTrigger>}
+                <DialogContent className="sm:max-w-[425px] overflow-y-auto max-h-screen">
+                    <DialogHeader>
+                        <DialogTitle>{title}</DialogTitle>
+                        {description && <DialogDescription>{description}</DialogDescription>}
+                    </DialogHeader>
+                    {children}
+                </DialogContent>
+            </Dialog>
+        );
+    }
 
-	return (
-		<Drawer open={isOpen} onOpenChange={setIsOpen}>
-			{trigger && <DrawerTrigger>{trigger}</DrawerTrigger>}
-			<DrawerContent>
-				<DrawerHeader className='text-left'>
-					<DrawerTitle>{title}</DrawerTitle>
-					{description && <DrawerDescription>{description}</DrawerDescription>}
-				</DrawerHeader>
-				{children}
-				<DrawerFooter className='pt-2'>
-					<DrawerClose asChild>
-						<Button variant='outline'>Cancelar</Button>
-					</DrawerClose>
-				</DrawerFooter>
-			</DrawerContent>
-		</Drawer>
-	);
+    return (
+        <Drawer open={isOpen} onOpenChange={setIsOpen}>
+            {trigger && <DrawerTrigger>{trigger}</DrawerTrigger>}
+            <DrawerContent>
+                <DrawerHeader className="text-left">
+                    <DrawerTitle>{title}</DrawerTitle>
+                    {description && <DrawerDescription>{description}</DrawerDescription>}
+                </DrawerHeader>
+                <ScrollArea className="overflow-y-auto">
+                    {children}
+                    <DrawerFooter className="pt-2">
+                        <DrawerClose asChild>
+                            <Button variant="outline">Cancelar</Button>
+                        </DrawerClose>
+                    </DrawerFooter>
+                </ScrollArea>
+            </DrawerContent>
+        </Drawer>
+    );
 }
